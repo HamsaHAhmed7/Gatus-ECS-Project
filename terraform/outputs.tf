041fa4e283dead67ec50cd280536fabb810e82ee
hamsa <hamsahahmed7@gmail.com>
2025-08-06 20:56:52 +0100

    outputs.tf added within terraform/ root

---
+++ terraform/outputs.tf
@@ -0,0 +1,23 @@
+#ALB Outputs
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.alb.alb_dns_name
+}
+
+#ECS Cluster Outputs
+output "ecs_cluster_name" {
+  description = "Name of the ECS Cluster"
+  value       = module.ecs_cluster.cluster_name
+}
+
+#ECS Task Definition 
+output "task_definition_arn" {
+  description = "ARN of the ECS Task Definition"
+  value       = module.ecs_task.task_definition_arn
+}
+
+#IAM Outputs 
+output "execution_role_arn" {
+  description = "ARN of the ECS task execution role"
+  value       = module.iam.execution_role_arn
+}
