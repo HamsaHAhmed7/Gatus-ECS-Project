c6ee0df1c6897ab1e42d10977bf9d25bba734810
hamsa <hamsahahmed7@gmail.com>
2025-08-07 22:15:59 +0100

    backend.tf added

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "gatus-tf-state-hamsa"
+    key            = "envs/prod/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "gatus-tf-locks"
+    encrypt        = true
+  }
+
+}
+
+
+ 
