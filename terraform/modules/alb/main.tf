36ae0cfcc8de2b1d6b8252a8d2a0566e61e5b818
hamsa <hamsahahmed7@gmail.com>
2025-08-08 09:58:53 +0100

    fix for 503 error on domain

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -21,12 +21,13 @@ resource "aws_lb_target_group" "gatus" {
 
 health_check {
   path                = "/"
+  matcher             = "200-399"
   interval            = 30
   timeout             = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
-  matcher             = "200"
+  healthy_threshold   = 3
+  unhealthy_threshold = 3
 }
+
 
 }
 
