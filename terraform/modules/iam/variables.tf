60b44d9e66da53f94818556e2933fc6936ced134
hamsa <hamsahahmed7@gmail.com>
2026-01-27 22:54:19 +0000

    chore: standardize module variable file naming and remove unused files
    - Rename variable.tf to variables.tf across all modules for consistency
    - Remove tfplan from version control
    - Delete unused Docker README.md
    - Remove unused IAM module variables.tf

--- terraform/modules/iam/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "create_task_role" {
-  description = "Whether to create a task role for the ECS task"
-  type        = bool
-  default     = false
-}
--- terraform/modules/sg/variables.tf
+++ terraform/modules/sg/variables.tf
@@ -1,5 +1,4 @@
 variable "vpc_id" {
   description = "The ID of the VPC where the security group will be created."
   type        = string
-  default = "vpc-0c99b83f91ca5ab91" 
 }
